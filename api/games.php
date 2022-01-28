fd2cfd666103e2ca5e20d872836e2b96a145a442
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2022-01-28 00:07:14 -0500

    fixed API

--- api/games.php
+++ api/games.php
@@ -3,35 +3,36 @@
     class gamesAPI {
 
         function Select($gameID='%', $limitResults=10) {
-            require_once 'database.php';
+            require_once '../database.php';
 
             $games = array();
-            $query = "SELECT `Id`, `Name`, `Cost` FROM `Games` WHERE `Id` LIKE :gameID ORDER BY `Id` DESC LIMIT 0, :limitResults;";
+            $query = "SELECT * FROM Games WHERE `Id` LIKE :gameID ORDER BY `Id` DESC LIMIT 0, :limitResults;";
             
             try {
                 $statement = $db->prepare($query);
+                $statement->bindValue(':gameID', $gameID, PDO::PARAM_STR);
+                $statement->bindValue(':limitResults', $limitResults, PDO::PARAM_INT);
+                $statement->execute();
+                $gamesResults = $statement->fetchAll();
+                $i = 0;
+                $games = array();
+                foreach( $gamesResults as $gameRow) {
+                    $i++;
+                    $games[$i] = array(
+                        'game_id' => $gameRow['Id'],
+                        'game_name'    => $gameRow['Name'],
+                        'game_cost'            => $gameRow['Cost']
+                    );
+                }
+
+                return json_encode($games);
             } catch(Exception $e) {
                 $message = $e->getMessage();
                 echo "An error occurred: " . $message;
                 exit;
             }
             
-            $statement->bindValue(':gameID', $gameID, PDO::PARAM_STR);
-            $statement->bindValue(':limitResults', $limitResults, PDO::PARAM_INT);
-            $statement->execute();
-            $gamesResults = $statement->fetchAll();
-            $i = 0;
-            $games = array();
-            foreach( $gamesResults as $priceRow) {
-                $i++;
-                $games[$i] = array(
-                    'game_id' => $priceRow['Id'],
-                    'game_name'    => $priceRow['Name'],
-                    'game_cost'            => $priceRow['Cost']
-                );
-            }
-
-            return json_encode($games);
+            
         }
 
     }
